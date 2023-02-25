ea529f694c53927e5ece97e408ac4b2554590d90
Nguyen Tran <merlinvn@gmail.com>
2023-02-25 15:29:14 -0500

    fix build deploy

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ import rehypePrism from '@mapbox/rehype-prism'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
+  images: { unoptimized: true },
   experimental: {
     scrollRestoration: true,
   },
