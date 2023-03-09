4b3ede91f8c13dd09b29e4201d199b0a570a8425
Nguyen Tran <merlinvn@gmail.com>
2023-03-08 20:32:22 -0500

    fix utilities link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ export function Footer() {
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/publications">Publications</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
+                <NavLink href="/utilities">Utilities</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Tran Dang Nguyen. All rights
